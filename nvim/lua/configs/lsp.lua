a25c43719b10eeb14a398f5fbea4f9c63fc35f4d
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-08-08 21:36:11 -0400

    Add lua lsp support

--- nvim/lua/configs/lsp.lua
+++ nvim/lua/configs/lsp.lua
@@ -3,7 +3,14 @@ require("mason-lspconfig").setup()
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+-- LUA
+require("lspconfig").lua_ls.setup {
+    capabilities = capabilities,
+}
+
 -- PYTHON
 require("lspconfig").pyright.setup {
     capabilities = capabilities,
 }
+
+
