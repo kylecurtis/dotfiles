eb040b6ddb16b0fbf4473a8fc5ff334fb04fa723
Kyle Curtis <30717363+kylecurtis@users.noreply.github.com>
2024-03-02 23:26:06 -0500

    Refactor neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,443 @@
--- CORE
-require("keymaps")
-require("plugins")
-require("settings")
+-- NOTE: GENERAL VIM SETTINGS
+vim.g.mapleader = " " -- set <space> as the leader key
+vim.g.maplocalleader = " " -- set <space> as the leader key
 
--- CONFIGS
-require("configs.lsp")
-require("configs.lualine")
-require("configs.treesitter")
-require("configs.telescope")
-require("configs.completions")
+vim.opt.number = true -- make line numbers default
+-- vim.opt.relativenumber = true -- relative line numbers
+
+vim.opt.mouse = "a" -- enable mouse mode (can be useful for resizing splits)
+vim.opt.showmode = false -- don't show the mode, since it's already in status line
+vim.opt.clipboard = "unnamedplus" -- sync clipboard between os and neovim.
+vim.opt.breakindent = true -- enable break indent
+vim.opt.undofile = true -- save undo history
+
+-- case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.signcolumn = "yes" -- keep signcolumn on by default
+
+-- decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- sets how neovim will display certain whitespace in the editor.
+-- See `:help list` and `:help listchars`
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- preview substitutions live, as you type
+vim.opt.inccommand = "split"
+
+-- show which line your cursor is on
+vim.opt.cursorline = false
+
+-- minimal number of screen lines to keep above and below the cursor
+vim.opt.scrolloff = 10
+
+-- set highlight on search, but clear on pressing <Esc> in normal mode
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- tab settings
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- NOTE: CUSTOM KEYBINDS
+vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
+
+-- diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- keybinds to make split navigation easier.
+-- use ctrl+<hjkl> to switch between windows
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+-- NOTE: HIGHLIGHT ON YANK
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- NOTE: BOOTSTRAP LAZY.NVIM
+-- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- WARNING: HERE IS WHERE YOU INSTALL YOUR PLUGINS VIA LAZY.
+require("lazy").setup({
+
+	-- TODO COMMENTS
+	{ "folke/todo-comments.nvim", dependencies = { "nvim-lua/plenary.nvim" }, opts = { signs = false } },
+
+	-- DETECT TABSTOP AND SHIFTWIDTH AUTOMATICALLY
+	{ "tpope/vim-sleuth" },
+
+	-- COMMENT PLUGIN FOR NEOVIM
+	{ "numToStr/Comment.nvim", opts = {}, lazy = false },
+
+	-- Useful plugin to show you pending keybinds.
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		config = function() -- This is the function that runs, AFTER loading
+			require("which-key").setup()
+			require("which-key").register({
+				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			})
+		end,
+	},
+
+	-- LUALINE
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {}, -- same as calling require('lualine').setup({})
+	},
+
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VeryLazy",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				-- `build` is used to run some command when the plugin is installed/updated.
+				build = "make", -- This is only run then, not every time Neovim starts up.
+				cond = function() -- `cond` is a condition used to determine whether this plugin
+					-- should be installed and loaded
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			{ "nvim-telescope/telescope-ui-select.nvim" },
+
+			-- Useful for getting pretty icons, but requires special (NERD) font.
+			{ "nvim-tree/nvim-web-devicons" },
+		},
+
+		-- TELESCOPE
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown(),
+					},
+				},
+			})
+
+			-- Enable telescope extensions, if they are installed
+			pcall(require("telescope").load_extension, "fzf")
+			pcall(require("telescope").load_extension, "ui-select")
+
+			-- See `:help telescope.builtin`
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+
+			-- Slightly advanced example of overriding default behavior and theme
+			vim.keymap.set("n", "<leader>/", function()
+				-- You can pass additional configuration to telescope to change theme, layout, etc.
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "[/] Fuzzily search in current buffer" })
+
+			vim.keymap.set("n", "<leader>s/", function()
+				builtin.live_grep({
+					grep_open_files = true,
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "[S]earch [/] in Open Files" })
+			vim.keymap.set("n", "<leader>sn", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, { desc = "[S]earch [N]eovim files" })
+		end,
+	},
+
+	-- LSPCONFIG
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			{ "j-hui/fidget.nvim", opts = {} },
+		},
+		config = function()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+				callback = function(event)
+					local map = function(keys, func, desc)
+						vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+					end
+					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+					map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+					map(
+						"<leader>ws",
+						require("telescope.builtin").lsp_dynamic_workspace_symbols,
+						"[W]orkspace [S]ymbols"
+					)
+					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+					map("K", vim.lsp.buf.hover, "Hover Documentation")
+					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+					local client = vim.lsp.get_client_by_id(event.data.client_id)
+					if client and client.server_capabilities.documentHighlightProvider then
+						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.document_highlight,
+						})
+						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+							buffer = event.buf,
+							callback = vim.lsp.buf.clear_references,
+						})
+					end
+				end,
+			})
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+			-- WARNING: SERVER SETTINGS HERE
+			local servers = {
+				-- rust
+				rust_analyzer = {},
+
+				-- python
+				pyright = {},
+				ruff = {},
+				ruff_lsp = {},
+
+				-- lua
+				lua_ls = {
+					settings = {
+						Lua = {
+							runtime = { version = "LuaJIT" },
+							workspace = {
+								checkThirdParty = false,
+								library = {
+									"${3rd}/luv/library",
+									unpack(vim.api.nvim_get_runtime_file("", true)),
+								},
+							},
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+				},
+			}
+			-- WARNING: MASON SETUP HERE
+			require("mason").setup()
+			local ensure_installed = vim.tbl_keys(servers or {})
+			vim.list_extend(ensure_installed, {
+				"stylua",
+				"rust_analyzer",
+				"pyright",
+				"ruff",
+				"ruff_lsp",
+			})
+			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+			require("mason-lspconfig").setup({
+				handlers = {
+					function(server_name)
+						local server = servers[server_name] or {}
+						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+						require("lspconfig")[server_name].setup(server)
+					end,
+				},
+			})
+		end,
+	},
+
+	-- conform (formatting)
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			notify_on_error = false,
+			format_on_save = {
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff" },
+			},
+		},
+	},
+
+	-- nvim cmp (for code completion)
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{
+				"L3MON4D3/LuaSnip",
+				build = (function()
+					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+						return
+					end
+					return "make install_jsregexp"
+				end)(),
+			},
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+		},
+
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			luasnip.config.setup({})
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+
+				completion = { completeopt = "menu,menuone,noinsert" },
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete({}),
+					["<C-l>"] = cmp.mapping(function()
+						if luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						end
+					end, { "i", "s" }),
+					["<C-h>"] = cmp.mapping(function()
+						if luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						end
+					end, { "i", "s" }),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
+				},
+			})
+		end,
+	},
+
+	-- MINI SURROUND
+	{
+		"echasnovski/mini.nvim",
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
+
+	-- TREESITTER
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "rust", "python", "bash", "c", "html", "lua", "markdown", "vim", "vimdoc" },
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+
+	-- NOTE: THEME IMPORTS GO HERE
+	{ "catppuccin/nvim", lazy = false, name = "catppuccin", priority = 1000 }, -- catppuccin
+	{ "folke/tokyonight.nvim", lazy = false, priority = 1000, opts = {} }, -- tokyo night
+	{ "rebelot/kanagawa.nvim", lazy = false, priority = 1000, opts = {} }, -- kanagawa
+	{ "EdenEast/nightfox.nvim", lazy = false, priority = 1000, opts = {} }, -- nightfox
+
+	-- aura
+	{
+		"baliestri/aura-theme",
+		lazy = false,
+		priority = 1000,
+		config = function(plugin)
+			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+			vim.cmd([[colorscheme aura-dark]])
+		end,
+	},
+
+	--[[
+	-- See `:help gitsigns` to understand what the configuration keys do
+	-- Adds git related signs to the gutter, as well as utilities for managing changes
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+	--]]
+}) -- NOTE: END OF LAZY SETUP
+
+-- WARNING: SET COLOR SCHEMES (KEEP ME HERE)
+-- ============================================================================
+
+-- INFO: BACKGROUND SETTINGS
+vim.o.background = "dark"
+-- vim.o.background = ""
+
+-- INFO: default: mocha
+-- vim.cmd([[colorscheme catppuccin]])
+
+-- INFO: night, storm, day, moon
+-- vim.cmd([[colorscheme tokyonight]])
+
+-- INFO: wave, dragon, lotus
+-- vim.cmd([[colorscheme kanagawa-wave]])
+
+-- INFO: nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
+vim.cmd([[colorscheme carbonfox]])
+
+-- NOTE: dark, dark-soft-text, soft-dark, soft-dark-soft-text
+-- vim.cmd([[colorscheme aura-dark]])
+
+-- WARNING: SET BG TRANSPARENCY (KEEP ME AT VERY BOTTOM)
+vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
+vim.cmd([[highlight NonText guibg=NONE ctermbg=NONE]])
